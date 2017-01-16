717db87e3d1b1beaab29ae0c152bfeede8b5c465
bstarling <benjamin.starling@gmail.com>
2017-01-15 19:12:47 -0500

    Add shell config file

---
+++ credentials.py
@@ -0,0 +1,10 @@
+# Twitter API token/keys
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+# AWS Config
+# or your settings here
+access_secret = ''
+access_id = ''
